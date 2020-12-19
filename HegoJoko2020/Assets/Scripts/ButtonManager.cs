b75557f100167d47694c56100e82894296aacf28
Alexandre Quéré <alex.quere64@gmail.com>
2020-12-19 20:36:04 +0100

    Add principal menu with working new game and quit game button

---
+++ HegoJoko2020/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void NewGameBtn(string newGameLevel) {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void ExitGameBtn() {
+        Application.Quit();
+    }
+}
