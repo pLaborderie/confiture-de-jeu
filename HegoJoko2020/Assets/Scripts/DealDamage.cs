1099445abe475235e7230d32d30bd1d7fd876abd
Antoine CHAMPAULT <antoine.champault@hotmail.com>
2020-12-19 23:28:25 +0100

    healthbare

---
+++ HegoJoko2020/Assets/Scripts/DealDamage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamage : MonoBehaviour 
+{
+	public void UpdateHealth (int dam)
+	{
+		PlayerHealth playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+		playerStats.TakeDamage(dam);
+	}
+
+	public void UpdateHealthS (int dam)
+    {
+		PlayerHealthS playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthS>();
+		playerStats.TakeDamage(dam);
+	}
+}
+
---
+++ HegoJoko2020/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthBar;
+
+    PlayerHealth playerHealth;
+
+
+    void Start()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        healthBar.value = playerHealth.health;
+
+    }
+
+    void Update()
+    {
+        healthBar.value = playerHealth.health;
+    }
+}
+
+
+
