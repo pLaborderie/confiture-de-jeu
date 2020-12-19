12aa3cd25048c51a0dccdcf51c7aa0dfdc503907
Paul Laborderie <paul.laborderie@gmail.com>
2020-12-19 23:51:47 +0100

    Fixing extra curly brace

--- HegoJoko2020/Assets/Scripts/DealDamage.cs
+++ HegoJoko2020/Assets/Scripts/DealDamage.cs
@@ -1,21 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class DealDamage : MonoBehaviour 
+public class DealDamage : MonoBehaviour
 {
 
-	public void UpdateHealth (int dam)
+  public void UpdateHealth(int dam)
 
-	{
-		PlayerHealth playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
-		playerStats.TakeDamage(dam);
-	}
+  {
+    PlayerHealth playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    playerStats.TakeDamage(dam);
+  }
 
-	public void UpdateHealthS (int dam)
-    {
-		PlayerHealthS playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthS>();
-		playerStats.TakeDamage(dam);
-	}
-}
+  public void UpdateHealthS(int dam)
+  {
+    PlayerHealthS playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthS>();
+    playerStats.TakeDamage(dam);
+  }
 }
