33f5b2efb7c56b2ae96be8be694eddb309513a22
Alexandre Quéré <alex.quere64@gmail.com>
2020-12-19 21:53:35 +0100

    Add input escape to pause the game

--- HegoJoko2020/Assets/Scripts/ButtonManager.cs
+++ HegoJoko2020/Assets/Scripts/ButtonManager.cs
@@ -3,11 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    public PauseMenu pauseFunction;
+
     public void NewGameBtn(string newGameLevel) {
         SceneManager.LoadScene(newGameLevel);
     }
 
     public void ExitGameBtn() {
         Application.Quit();
+    }
+
+    public void LoadGameBtn() {
+        //
     }
 }
