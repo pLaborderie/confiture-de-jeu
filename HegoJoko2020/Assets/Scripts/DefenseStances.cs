47b05eaf1ddca9445be9f155cf06c1c8cd00ed27
Enzo Moulian <enzomoulian@gmail.com>
2020-12-19 22:41:25 +0100

    Ajout des coups et des positions de défense

---
+++ HegoJoko2020/Assets/Scripts/DefenseStances.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public enum AllDefenseStances
+{
+    UpBlock,
+    DownBlock,
+    UpDodge,
+    DownDodge
+}
+
+public class DefenseStances : MonoBehaviour
+{
+    public AllDefenseStances[] allDefenseStances;
+
+    private Dictionary<AllDefenseStances, AllHits[]> hitBlocked;
+    private Dictionary<AllDefenseStances, AllHits[]> hitDodged;
+    private Dictionary<AllDefenseStances, float> f_damageReduction;
+
+    private AllDefenseStances currentDefenseStance;
+
+    void Start()
+    {
+        for (int i = 0; i < allDefenseStances.Length; i++)
+        {
+            switch (allDefenseStances[i])
+            {
+                case AllDefenseStances.UpBlock:
+                    hitBlocked.Add(allDefenseStances[i], new AllHits[] { AllHits.UpJab, AllHits.UpCross, AllHits.Uppercut });
+                    hitDodged.Add(allDefenseStances[i], new AllHits[0]);
+                    f_damageReduction.Add(allDefenseStances[i], 10);
+                    break;
+                case AllDefenseStances.DownBlock:
+                    hitBlocked.Add(allDefenseStances[i], new AllHits[] { AllHits.DownJab, AllHits.DownCross });
+                    hitDodged.Add(allDefenseStances[i], new AllHits[0]);
+                    f_damageReduction.Add(allDefenseStances[i], 10);
+                    break;
+                case AllDefenseStances.UpDodge:
+                    hitBlocked.Add(allDefenseStances[i], new AllHits[0]);
+                    hitDodged.Add(allDefenseStances[i], new AllHits[] { AllHits.UpJab, AllHits.UpCross, AllHits.Uppercut });
+                    f_damageReduction.Add(allDefenseStances[i], 0);
+                    break;
+                case AllDefenseStances.DownDodge:
+                    hitBlocked.Add(allDefenseStances[i], new AllHits[0]);
+                    hitDodged.Add(allDefenseStances[i], new AllHits[] { AllHits.DownJab, AllHits.DownCross});
+                    f_damageReduction.Add(allDefenseStances[i], 0);
+                    break;
+            }
+        }
+    }
+
+    public void ReceiveHit(AllHits hitReceived)
+    {
+        if (ArrayUtility.Contains<AllHits>(hitDodged[currentDefenseStance], hitReceived))
+        {
+            // Il faut dodge le coup
+        }
+        else if (ArrayUtility.Contains<AllHits>(hitBlocked[currentDefenseStance], hitReceived))
+        {
+            //gameObject.TakeDamage(hitReceived.f_hitPower - currentDefenseStance.f_damageReduction);
+        }
+        else
+        {
+            //gameObject.TakeDamage(hitReceived.f_hitPower);
+        }
+    }
+}
---
+++ HegoJoko2020/Assets/Scripts/Hits.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public enum AllHits
+{
+    UpJab,
+    DownJab,
+    UpCross,
+    DownCross,
+    Uppercut
+}
+
+public class Hits : MonoBehaviour
+{
+    public AllHits[] allHits;
+
+    private Dictionary<AllHits, int> maxUse;
+    private Dictionary<AllHits, int> currentUse;
+    private Dictionary<AllHits, int> hitPower;
+
+    private GameManager gameManager;
+
+    void Start()
+    {
+        for(int i = 0; i < allHits.Length; i++)
+        {
+            switch (allHits[i])
+            {
+                case AllHits.UpJab:
+                    maxUse.Add(allHits[i],3);
+                    hitPower.Add(allHits[i], 10);
+                    currentUse.Add(allHits[i], maxUse[allHits[i]]);
+                    break;
+                case AllHits.DownJab:
+                    maxUse.Add(allHits[i], 3);
+                    hitPower.Add(allHits[i], 10);
+                    currentUse.Add(allHits[i], maxUse[allHits[i]]);
+                    break;
+                case AllHits.UpCross:
+                    maxUse.Add(allHits[i], 3);
+                    hitPower.Add(allHits[i], 20);
+                    currentUse.Add(allHits[i], maxUse[allHits[i]]);
+                    break;
+                case AllHits.DownCross:
+                    maxUse.Add(allHits[i], 3);
+                    hitPower.Add(allHits[i], 20);
+                    currentUse.Add(allHits[i], maxUse[allHits[i]]);
+                    break;
+                case AllHits.Uppercut:
+                    maxUse.Add(allHits[i], 3);
+                    hitPower.Add(allHits[i], 40);
+                    currentUse.Add(allHits[i], maxUse[allHits[i]]);
+                    break;
+            }
+        }
+
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    public void DealHit(AllHits hit)
+    {
+        if (ArrayUtility.Contains<AllHits>(allHits, hit))
+        {
+            if (currentUse[hit] > 0)
+            {
+                gameManager.GetOpponentOf(gameObject).GetComponent<DefenseStances>().ReceiveHit(hit);
+            }
+        }
+        else
+        {
+            Debug.Log("This fighter doesn't know how to perform this hit...");
+        }
+    }
+}
