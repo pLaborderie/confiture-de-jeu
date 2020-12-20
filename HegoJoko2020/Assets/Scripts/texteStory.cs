0cee37f2854fe0d8a7cbb8037be4cf1c10a0fd85
Antoine CHAMPAULT <antoine.champault@hotmail.com>
2020-12-20 18:40:46 +0100

    defiler texte

---
+++ HegoJoko2020/Assets/Scripts/texteStory.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class texteStory : MonoBehaviour
+{
+
+    private Text uiText;
+
+    private float showSpeed = 0.05f;
+
+    private string showText, uiTextCopy;
+
+    private bool coroutineProtect, loadText;
+
+
+    private void Start()
+    {
+        uiText = GetComponent<Text>();
+
+        TextInformations();
+    }
+
+    private void OnEnable() { uiTextCopy = null; }
+
+    private void Update()
+    {
+        if (loadText && !coroutineProtect)
+        {
+            StartCoroutine(LoadLetters(uiTextCopy));
+            coroutineProtect = true;
+        }
+
+        else if (loadText && coroutineProtect) { uiText.text = showText; }
+
+        else if (!loadText && !coroutineProtect)
+        {
+            if (uiText.text != uiTextCopy) { TextInformations(); }
+        }
+    }
+
+    private void TextInformations()
+    {
+        uiTextCopy = uiText.text;
+        showText = null;
+        uiText.text = null;
+
+        loadText = true;
+        coroutineProtect = false;
+    }
+
+    private IEnumerator LoadLetters(string completeText)
+    {
+        int textSize = 0;
+
+        while (textSize < completeText.Length)
+        {
+            showText += completeText[textSize++];
+            yield return new WaitForSeconds(showSpeed);
+        }
+
+        coroutineProtect = false;
+        loadText = false;
+    }
+
+}
