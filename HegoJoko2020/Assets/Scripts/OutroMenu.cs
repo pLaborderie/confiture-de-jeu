87e077e1358875c17d7a52b3de146fe8e2f0fcfa
Alexandre Quéré <alex.quere64@gmail.com>
2020-12-21 16:22:06 +0100

    Add phrase for win outro

--- HegoJoko2020/Assets/Scripts/OutroMenu.cs
+++ HegoJoko2020/Assets/Scripts/OutroMenu.cs
@@ -8,8 +8,23 @@ using UnityEngine.UI;
 public class OutroMenu : MonoBehaviour
 {
     public string[] citationsWin = new string[] {
-                                            "En boxe, vous créez une stratégie pour battre chaque nouvel adversaire, c’est comme les échecs."
+                                            "Pourquoi être aussi corrompu ? Education de m*** ! Aucun honneur ! Aucune dignité ! Bon… vous avez gagné mais quand même !",
+                                            "Et bah voilà c’est pas compliqué d’y arriver quand on met les moyens !",
+                                            "Vous avez dépensé beaucoup mais cette combine vous rapportera gros !",
+                                            "Le prochain match est dans quelques instants ! J’espère que votre petit truc marchera encore !",
+                                            "Un double KO ? Non… Je suis certain que c’est une pure.. coïncidence !"
                                             };
+
+
+
+
+
+
+
+
+
+
+
     public string[] citationsLoose = new string[] {
                                             "Je crois que vous avez oublié quelques billets quand vous avez rempli cette mallette !", 
                                             "Hum… Un concurrent semble avoir mis plus d’argent sur le tapis ! Un de vos joueurs ne s’est pas couché !", 
